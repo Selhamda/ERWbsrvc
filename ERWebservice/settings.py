bb4cb237eee5c8b30e51679e936415249cdb8f0f
Salim El <salimelhamdani@htmail.com>
2018-04-20 00:03:02 +0200

    Changes for Heroku

--- ERWebservice/settings.py
+++ ERWebservice/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5mff^-gucwvy2$&^4*d8tx7i(2lemrocjjl(@*l+epz&jd+ahj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -78,13 +80,16 @@ WSGI_APPLICATION = 'ERWebservice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'easy_ride',
-        'USER' : 'salim',
+        'NAME': 'easy_api',
+        'USER' : 'salimeche',
         'PASSWORD' : 'easyride123',
         'HOST': 'localhost',
         'port': '',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
