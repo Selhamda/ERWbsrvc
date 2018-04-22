b604ae67df72ecf3a23980565ef4a69580fff469
Salim El <salimelhamdani@htmail.com>
2018-04-22 16:32:32 +0200

    update for db credentials security

--- ERWebservice/settings.py
+++ ERWebservice/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'ERWebservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'easy_db',
-        'USER' : 'salimeche',
-        'PASSWORD' : 'easyride321',
-        'HOST': 'localhost',
-        'port': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': os.environ.get('DATABASE_PORT', ''),
     }
 }
 
