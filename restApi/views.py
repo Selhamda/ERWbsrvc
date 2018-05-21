c0df257c0707972a1b60abeeab738ffcb396811f
Salim El <salimelhamdani@htmail.com>
2018-05-21 03:00:20 +0200

    http404 handling for ulv post

--- restApi/views.py
+++ restApi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django.shortcuts import get_object_or_404
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseNotFound
 from restApi.models import Utilisateur, Voiture, Utilisateur_loue_voiture
 from restApi.serializers import FullVoitureSerializer, ConsoVoitureSerializer, FullUtilisateurSerializer, ULVSerializer
 # Create your views here.
@@ -27,7 +27,7 @@ class ULVCreateView(generics.CreateAPIView):
         try:
             voit_id = Voiture.objects.get(matricule=mat).car_id
         except Voiture.DoesNotExist:
-            return Http404("Pas de voiture de matricule"+mat+"trouvée")
+            return HttpResponseNotFound('<h1>Voiture not found</h1>')
         request.data["voiture"] = voit_id
         return super(ULVCreateView,self).create(request,*args,**kwargs)
 
